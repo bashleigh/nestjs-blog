4f8cbf89d4eb3df5445db426b0bde478421736ef
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-10-01 15:30:01 +0100

    Fixed issues with database params

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,9 +4,9 @@ export default {
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  entities: process.env.ENTITIES,
+  entities: process.env.DB_ENTITIES,
   synchronize: process.env.DB_SYNCRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
   migrationsRun: process.env.DB_MIGRATION_RUN === 'true',
-  migrationsDir: process.env.DB_MIGRATION_DIR === 'true',
+  migrationsDir: process.env.DB_MIGRATION_DIR,
 };
