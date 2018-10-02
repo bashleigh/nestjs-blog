885956235fae6897d4ac2f9139f4707fbeb0fe40
ashleigh <ashleigh@wi-q.com>
2018-10-02 15:18:21 +0100

    Removed unused classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from 'nestjs-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
@@ -13,7 +11,5 @@ import * as path from 'path';
       inject: [ConfigService],
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
