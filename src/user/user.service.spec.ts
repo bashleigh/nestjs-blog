482aacc4069d4bffa5f49888b196fd740daac7a4
Aaryanna Simonelli <ashleighsimonelli@gmail.com>
2018-10-12 21:36:42 +0100

    Added tests for userService

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -5,13 +5,17 @@ import * as path from 'path';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pagination } from '../paginate';
 import { INestApplication } from '@nestjs/common';
+import { UserEntity } from '../entities';
+import { UpdateResult } from 'typeorm';
 
 describe('UserService', () => {
   let module: TestingModule;
   let userService: UserService;
   let app: INestApplication;
+  let user: UserEntity;
+  let auth: Partial<UserEntity>;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     module = await Test.createTestingModule({
       imports: [
         ConfigModule.load(path.resolve(__dirname, '../', 'config', '*.ts')),
@@ -33,6 +37,54 @@ describe('UserService', () => {
       limit: 10,
       page: 0,
     })).toBeInstanceOf(Pagination);
+  });
+
+  it('Creation', async () => {
+    const number = Math.floor(Math.random() * Math.floor(20));
+    expect(user = await userService.create({
+      email: `test${number}@test.com`,
+      firstname: 'test',
+      lastname: 'test',
+      password: 'password',
+    })).toBeInstanceOf(UserEntity);
+  });
+
+  it('Update', async () => {
+
+    user.firstname = 'updated';
+
+    const result = await userService.update(user);
+    expect(result).toBeInstanceOf(UpdateResult);
+    expect(user.firstname).toEqual('updated');
+  });
+
+  it('findByEmailWithPassword', async () => {
+    auth = await userService.findByEmailWithPassword(user.email);
+
+    expect(auth).toHaveProperty('password');
+  });
+
+  it('CompareHash', async () => {
+    const result = await userService.compareHash('password', auth.password);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('findByEmail', async () => {
+    const result = await userService.findByEmail(user.email);
+
+    expect(result).toBeInstanceOf(UserEntity);
+
+    delete user.updated;
+
+    expect(result).toEqual(expect.objectContaining(user));
+  });
+
+  it('findById', async () => {
+    const result = await userService.findById(user.id);
+
+    expect(result).toBeInstanceOf(UserEntity);
+    expect(result).toEqual(expect.objectContaining(user));
   });
 
   afterAll(() => app.close());
